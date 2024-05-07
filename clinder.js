11a8464facb82fcad3bc674c0686c9ec2b88a6a9
bonnbear <59333557+bonnbear@users.noreply.github.com>
2024-05-07 23:35:09 +0800

    Update clinder.js

--- clinder.js
+++ clinder.js
@@ -3,17 +3,15 @@
     <el-popover
       ref="popoverRef"
       placement="bottom-start"
-      trigger="manual"
+      trigger="click"
       v-model:visible="popoverVisible"
       :width="popoverWidth"
       popper-class="custom-popover"
     >
-      <!-- 将输入框放置在这里，作为触发弹出层的元素 -->
       <template #reference>
         <el-input
           v-model="selectedLabel"
           placeholder="Please input"
-          @focus="showPopover"
           @input="filterTextChange"
         />
       </template>
@@ -22,13 +20,14 @@
         ref="treeRef"
         :data="data"
         :props="defaultProps"
-        default-expand-all
+        :expand-on-click-node="false"
         :filter-node-method="filterNode"
         @node-click="handleNodeClick"
       />
     </el-popover>
   </div>
 </template>
+
 <script setup>
 import { ref, watch, nextTick } from 'vue'
 import { ElTree } from 'element-plus'
@@ -62,19 +61,12 @@ const handleNodeClick = (data) => {
   popoverVisible.value = false
 }
 
-const showPopover = () => {
-  popoverVisible.value = true
-  nextTick(() => {
-    popoverWidth.value = popoverRef.value.width
-  })
-}
-
-const hidePopover = () => {
-  popoverVisible.value = false
-}
-
 const filterTextChange = (val) => {
   filterText.value = val
+  popoverVisible.value = true
+  nextTick(() => {
+    popoverWidth.value = Math.max(popoverRef.value.width, treeRef.value.$el.scrollWidth + 20)
+  })
 }
 
 const data = [
@@ -131,7 +123,7 @@ const data = [
 
 <style scoped>
 .tree-select {
-  width: 240px;
+  width: 100%;
 }
 :deep(.custom-popover) {
   padding: 0;
@@ -143,137 +135,3 @@ const data = [
   overflow-y: auto;
 }
 </style>
-
-
-
-
-
-
-
-
-
-
-<template>
-  <div>
-    <el-input
-      v-model="selectedLabel"
-      placeholder="Please input"
-      style="width: 240px"
-      @focus="showPopover"
-      @blur="hidePopover"
-      @input="filterTextChange"
-    />
-    <el-popover
-      ref="popoverRef"
-      placement="bottom-start"
-      trigger="manual"
-      v-model:visible="popoverVisible"
-      :width="240"
-    >
-      <el-tree
-        ref="treeRef"
-        :data="data"
-        :props="defaultProps"
-        default-expand-all
-        :filter-node-method="filterNode"
-        @node-click="handleNodeClick"
-      />
-    </el-popover>
-  </div>
-</template>
-
-<script setup>
-import { ref, watch } from 'vue'
-import { ElTree } from 'element-plus'
-
-const selectedLabel = ref('')
-const filterText = ref('')
-const popoverVisible = ref(false)
-const popoverRef = ref(null)
-const treeRef = ref(null)
-
-const defaultProps = {
-  children: 'children',
-  label: 'label',
-}
-
-watch(filterText, (val) => {
-  if (treeRef.value) {
-    treeRef.value.filter(val)
-  }
-})
-
-const filterNode = (value, data) => {
-  if (!value) return true
-  return data.label.toLowerCase().includes(value.toLowerCase())
-}
-
-const handleNodeClick = (data) => {
-  selectedLabel.value = data.label
-  filterText.value = ''
-  popoverVisible.value = false
-}
-
-const showPopover = () => {
-  popoverVisible.value = true
-}
-
-const hidePopover = () => {
-  popoverVisible.value = false
-}
-
-const filterTextChange = (val) => {
-  filterText.value = val
-}
-
-const data = [
-  {
-    id: 1,
-    label: 'Level one 1',
-    children: [
-      {
-        id: 4,
-        label: 'Level two 1-1',
-        children: [
-          {
-            id: 9,
-            label: 'Level three 1-1-1',
-          },
-          {
-            id: 10,
-            label: 'Level three 1-1-2',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: 2,
-    label: 'Level one 2',
-    children: [
-      {
-        id: 5,
-        label: 'Level two 2-1',
-      },
-      {
-        id: 6,
-        label: 'Level two 2-2',
-      },
-    ],
-  },
-  {
-    id: 3,
-    label: 'Level one 3',
-    children: [
-      {
-        id: 7,
-        label: 'Level two 3-1',
-      },
-      {
-        id: 8,
-        label: 'Level two 3-2',
-      },
-    ],
-  },
-]
-</script>
